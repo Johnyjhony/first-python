f2e720ac4322a603b10ac7b856b963263707927d
Adrian Galvez <126625233+Johnyjhony@users.noreply.github.com>
2023-02-28 23:20:16 +0000

    Add main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-print ('Hello World')
+
+print ("Hello World!")
+print ('Hello Philippines!')
+if True:
+  print ('Hello World')
 
 num1 = 10
 num2 = 20
-
-num3 = num1 + num2
+num3 = num1 + num2 
 print (num3)
+
+message = 'Computer Literacy 101'
+print (message)
